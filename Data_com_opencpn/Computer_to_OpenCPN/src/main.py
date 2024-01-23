23fd39fd77979927b8ceeb861406113dd8bcf5fc
Alen Iriskic <cozyflexing@gmail.com>
2024-01-23 12:37:36 +0100

    merged code

--- Data_com_opencpn/Computer_to_OpenCPN/src/main.py
+++ Data_com_opencpn/Computer_to_OpenCPN/src/main.py
@@ -10,11 +10,11 @@ from ..custom_project_lib.nmea0183_sentences import NMEA0183_GEN as GEN
 from ..custom_project_lib.nmea0183_sentences_test import NMEA0183_GEN_TEST as TEST
 
 # [WINDOWS] Replace 'COMX' with the appropriate virtual COM port on your system
-serial_port = 'COM1'
+serial_port = "COM1"
 # [LINUX] Replace '/dev/ttyUSBX' with the appropriate serial port on your system
 # serial_port = '/dev/ttyUSB0'
 
-#lat and long in degree minutes format(DM) Between uk and ireland {Test}
+# lat and long in degree minutes format(DM) Between uk and ireland {Test}
 latitude = 53.679746945954754
 longitude = -5.167506462247125
 
@@ -30,27 +30,28 @@ while True:
     # Use any sentences here and send them to OpenCPN with serial.write((sentence + '\r\n').encode())
 
     # Create a minimal GGA sentence with only latitude and longitude
-    # gga = GEN.gga(time_data = None, 
-    #                             lat = latitude, 
-    #                             long = longitude, 
-    #                             fix_quality = 1, 
-    #                             satellites = 10, 
-    #                             horizontal_dilution_of_precision = 0.1, 
-    #                             elevation_above_sea_level = 255.747, 
-    #                             elevation_unit = "M", 
-    #                             geoid = -32.00, 
-    #                             geoid_unit = "M", 
-    #                             age_of_correction_data_seconds = "01", 
+    # gga = GEN.gga(time_data = None,
+    #                             lat = latitude,
+    #                             long = longitude,
+    #                             fix_quality = 1,
+    #                             satellites = 10,
+    #                             horizontal_dilution_of_precision = 0.1,
+    #                             elevation_above_sea_level = 255.747,
+    #                             elevation_unit = "M",
+    #                             geoid = -32.00,
+    #                             geoid_unit = "M",
+    #                             age_of_correction_data_seconds = "01",
     #                             correction_station_id = "0000")
 
-    gga = TEST.test_gga(latitude = latitude, longitude = longitude, print_sentence = True)
+    gga = TEST.test_gga(latitude=latitude, longitude=longitude, print_sentence=True)
     # Send the NMEA sentence to the serial port
-    ser.write((gga + '\r\n').encode())  # Ensure to add line ending (\r\n) for NMEA sentences
+    ser.write(
+        (gga + "\r\n").encode()
+    )  # Ensure to add line ending (\r\n) for NMEA sentences
 
     # Increment the latitude and longitude for the next update
     latitude += -0.001
     longitude += -0.009
-    
+
     # Wait for a few seconds before sending the next update
     time.sleep(0.5)
-
